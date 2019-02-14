eba9e5cb889c1bb6299e7f143e4957714032c376
unknown <estebanbotero1036@gmail.com>
2019-02-14 16:05:05 -0500

    Calificacion

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+     super();
+
+     this.state = {
+       repetidor:""
+     }
+  }
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" onChange={this.obtenerValor.bind(this)} />
+        <p className="repeater">{this.state.repetidor}</p>
       </div>
     );
+  }
+
+  obtenerValor(e){
+      this.setState({
+        repetidor: e.target.value
+      })
   }
 }
 
